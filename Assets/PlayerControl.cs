6d15772168dedf842429ae426eb7792869b31f64
unknown <simaomadiadia@gmail.com>
2025-11-17 22:13:43 +0000

    Configurar o limite da deslocacao da Nave, impossiblitar a nave sair na area de jogo(horizontalmente e verticalmente)

--- Assets/PlayerControl.cs
+++ Assets/PlayerControl.cs
@@ -1,23 +1,31 @@
+using System;
 using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
     
     public Rigidbody rb;
-    public float speed;
+
+    public float xmin, xmax,zmin,zmax;    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    {
+    { 
         
     }
      
      void FixedUpdate()
     {
+        //moviment Control
         float moveH =Input.GetAxis("Horizontal");
         float moveV =Input.GetAxis("Vertical");
         Vector3 moviment = new Vector3(moveH,0.0f,moveV);
         rb.linearVelocity = moviment *speed;
+
+        // area control
+        rb.position= new Vector3(Math.Clamp(rb.position.x,xmin,xmax) ,0.0f, Math.Clamp(rb.position.z,zmin,zmax));
     }
+    
+
     // Update is called once per frame
     void Update()
     {
